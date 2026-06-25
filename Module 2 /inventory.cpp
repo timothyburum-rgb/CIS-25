0be6dd9b6bdaf045e3de461b50bc15c9518c8423
timothyburum-rgb <timothy.burum@gmail.com>
2026-06-24 19:35:38 -0700

    Update inventory.cpp

--- Module 2 /inventory.cpp
+++ Module 2 /inventory.cpp
@@ -2,16 +2,9 @@
 using namespace std;
 int main() {
   cout << "Hello Inventory System!" << end1;
-  cout << "\nInventory Report:\n";
-  cout << "=\n";
-
-int ItemCount = 42;
-  cout << "\tItem Count (int):\t" << itemCount << "\n";
-float itemPrice = 9.99f;
-  cout << "\tItem Price (float):\t" << itemPrice << "\n";
-char itemGrade = 'A';
-  cout << "\tItem Grade (char):\t" << itemGrade << "n\";
-  cout << "\nEnd of Inventory Report. \n";
-
+  cout << "\tData Types:\n";
+  cout << "Size of int: " << sizeof(int) << " bytes\n";
+  cout << "Size of float: " << sizeof(float) << " bytes\n";
+  cout << "Size of char: " << sizeof(char) << " bytes\n";
 return 0;
 }
