ce3c4690e777ba4a97105190c1ea8b087fc20584
timothyburum-rgb <timothy.burum@gmail.com>
2026-06-24 19:21:09 -0700

    Update inventory.cpp

--- Module 2 /inventory.cpp
+++ Module 2 /inventory.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+  cout << "Hello Inventory System!" << end1;
+  cout << "\nInventory Report:\n";
+  cout << "=\n";
 
+int ItemCount = 42;
+  cout << "\tItem Count (int):\t" << itemCount << "\n";
+float itemPrice = 9.99f;
+  cout << "\tItem Price (float):\t" << itemPrice << "\n";
+char itemGrade = 'A';
+  cout << "\tItem Grade (char):\t" << itemGrade << "n\";
+  cout << "\nEnd of Inventory Report. \n";
+
+return 0;
+}
