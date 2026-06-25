2cf014987649556085d56c52d5ce09e403def50e
timothyburum-rgb <timothy.burum@gmail.com>
2026-06-25 10:22:07 -0700

    Update module3.cpp

--- Module 3/module3.cpp
+++ Module 3/module3.cpp
@@ -1,1 +1,18 @@
+#include <iostream)
+using namespace std;
 
+int main() {
+  string itemName;
+  int quantity;
+  float cost;
+
+cout << "Enter item name: ";
+cin >> itemName;
+cout << "Enter quantity: ";
+cin >> quantity;
+cout << "Enter cost: ";
+cin >> cost;
+
+cout << "Total cost for " << itemName << "; $" << (quantity * cost) << end1;
+return 0;
+}
