a79dafef4b5b3f23db4aa671e2ee91cf0bc0e9d2
timothyburum-rgb <timothy.burum@gmail.com>
2026-06-24 19:37:15 -0700

    Update inventory.cpp

--- Module 2 /inventory.cpp
+++ Module 2 /inventory.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 using namespace std;
 int main() {
-  cout << "Hello Inventory System!" << end1;
+  
+  cout << "Hello Inventory System!\n";
   cout << "\tData Types:\n";
   cout << "Size of int: " << sizeof(int) << " bytes\n";
   cout << "Size of float: " << sizeof(float) << " bytes\n";
   cout << "Size of char: " << sizeof(char) << " bytes\n";
+  
 return 0;
 }
