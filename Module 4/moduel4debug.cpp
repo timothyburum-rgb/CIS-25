c00434c99412f04e511141b0cdc2c5ea923dd367
timothyburum-rgb <timothy.burum@gmail.com>
2026-06-30 11:45:29 -0700

    Update moduel4debug.cpp

--- Module 4/moduel4debug.cpp
+++ Module 4/moduel4debug.cpp
@@ -3,5 +3,23 @@
 using namespace std;
 
 int() main {
-  
+  //added commas between each variable and ended the line with a semicolon insted of a comma
+  float n1, n1, n3;
+  cout << "Enter three numbers: ":
+
+  //changed > to >> and removed extra n1, added n2  
+  cin >> n1 >> n2 >> n3;
+
+  if (n1 >= n2 && n1 >= n3) {
+      cout << "Largest number: " << n1;
+}
+else {
+  if (n2 >= n1 && n2 >= n3) {
+    cout << "Largest number: " << n2;
+  }
+else {
+  cout << "Largest number: " << n3;
+  }
+}
+return 0;
 }
