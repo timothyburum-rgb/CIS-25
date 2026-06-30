b325d0faa7423139db8af38853afc61f477925f5
timothyburum-rgb <timothy.burum@gmail.com>
2026-06-30 12:54:54 -0700

    Update moduel4debug.cpp

--- Module 4/moduel4debug.cpp
+++ Module 4/moduel4debug.cpp
@@ -8,4 +8,17 @@ int() main {
   cout << "Enter three numbers: ";
   //changed > to >> and removed extra n1 variable, added n2
   cin >> n1 >> n2 >> n3;
+  //n1=n2 says n1 and n2 are equal, changed to n1>=n2 to compare and closed with parentheses which was missing
+  if (n1 >= n2 && n1 >= n3) {
+    cout << "Largest number: " << n1;
+  }
+else {
+  if (n2 >= n1 && n2 >= n3) {
+    cout << "Largest number: " << n2;
+  }
+  else {
+    cout << "Largest number: " << n3;
+  }
+}
+  return 0;
 }
