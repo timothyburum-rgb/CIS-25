0a497315a8b04c0ccd6268c77b01b75e553e4a4a
timothyburum-rgb <timothy.burum@gmail.com>
2026-06-29 18:58:20 -0700

    Update module4.cpp

--- Module 4/module4.cpp
+++ Module 4/module4.cpp
@@ -2,4 +2,21 @@
 #include <iostream>
 using namespace std;
 int main () {
-  
+  int choice;
+  cout << "Inventory Menu:\n";
+  cout << "1. Add Item\n";
+  cout << "2. Inspect Item\n";
+  cout << "3. Remove Item\n";
+  cout << "Enter Choice: ";
+  if (choice == 1) {
+  cout << "Adding Item...\n";
+  }
+  if (choice == 2) {
+  cout << "Inspecting Item...\n";
+  }
+  if (choice == 3) {
+  cout << "Deleting Item...\n";
+  }
+
+  return 0;
+}
