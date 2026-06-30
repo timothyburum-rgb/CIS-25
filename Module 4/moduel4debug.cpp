bf32a283ce14a2f4212ea1f1bd493e04cf2fb685
timothyburum-rgb <timothy.burum@gmail.com>
2026-06-30 11:49:23 -0700

    Update moduel4debug.cpp

--- Module 4/moduel4debug.cpp
+++ Module 4/moduel4debug.cpp
@@ -3,7 +3,9 @@
 using namespace std;
 
 int() main {
+  //added commas between each variable and ended the line with a semicolon instead of a comma
   float n1, n2, n3;
   cout << "Enter three numbers: ";
+  //changed > to >> and removed extra n1 variable, added n2
   cin >> n1 >> n2 >> n3;
 }
