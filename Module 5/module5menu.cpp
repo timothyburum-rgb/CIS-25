3c72057e6d8fa154758abc07ad084b9cf6fa69cd
timothyburum-rgb <timothy.burum@gmail.com>
2026-06-30 17:35:38 -0700

    Update module5menu.cpp

--- Module 5/module5menu.cpp
+++ Module 5/module5menu.cpp
@@ -1,1 +1,14 @@
 
+#include <iostream>
+using namespace std;
+void showMenu() {
+  cout << "1. Add\n2. View\n3. Exit\n";
+}
+int main() {
+  int option;
+  do {
+    showMenu();
+    cin >> option;
+  } while (option != 3);
+  return 0;
+}
