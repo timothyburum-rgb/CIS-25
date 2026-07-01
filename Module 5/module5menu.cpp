8de88ca4b468abce93d008f9410dd92ee5fd8ef0
timothyburum-rgb <timothy.burum@gmail.com>
2026-06-30 18:00:06 -0700

    Update module5menu.cpp

--- Module 5/module5menu.cpp
+++ Module 5/module5menu.cpp
@@ -1,14 +1,34 @@
 
 #include <iostream>
+#include <string>
 using namespace std;
+
 void showMenu() {
   cout << "1. Add\n2. View\n3. Exit\n";
 }
+
+void addItem() {
+  string itemName:
+  cout << "Enter item name: ";
+  cin >> itemName;
+  cout << itemName << " added.\n";
+}
+
+void viewInventory() {
+  cout << "Viewing inventory...\n";
+}
+
 int main() {
   int option;
   do {
     showMenu();
     cin >> option;
+    if (option == 1) {
+      addItem();
+    }
+    if (option == 2) {
+      viewInventory();
+    }
   } while (option != 3);
   return 0;
 }
