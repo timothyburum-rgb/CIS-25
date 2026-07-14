7d51de94614f5c50188b41dd6212c01876295ae8
timothyburum-rgb <timothy.burum@gmail.com>
2026-07-13 22:47:32 -0700

    Update module10.cpp

--- Module 10/module10.cpp
+++ Module 10/module10.cpp
@@ -1,4 +1,27 @@
 #include <iostream>
-#include <string>
 using namespace std;
-  
+
+class Product {
+  int id;
+  string name;
+  float price;
+public:
+//Constructor
+Product(int i, string n, float p) {
+  id = i;
+  name = n;
+  price = p;
+  cout << "Constructor called\n";
+}
+~Product() {
+  cout << "Destructor called\n";
+}
+void printDetails() {
+  cout << "ID:"<<id<<", Name:"<<name<<", Price: $"<<price<< endl;
+}
+
+int main() {
+  Product p(101, "Notebook", 4.99);
+  p.printDetails();
+  return 0;
+}
