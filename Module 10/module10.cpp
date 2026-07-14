c4e1002b7af0b2b2ca39cf4723715768f49bd092
timothyburum-rgb <timothy.burum@gmail.com>
2026-07-13 22:51:02 -0700

    Update module10.cpp

--- Module 10/module10.cpp
+++ Module 10/module10.cpp
@@ -2,26 +2,27 @@
 using namespace std;
 
 class Product {
-  int id;
-  string name;
-  float price;
+    int id;
+    string name;
+    float price;
 public:
-//Constructor
-Product(int i, string n, float p) {
-  id = i;
-  name = n;
-  price = p;
-  cout << "Constructor called\n";
-}
-~Product() {
-  cout << "Destructor called\n";
-}
-void printDetails() {
-  cout << "ID:"<<id<<", Name:"<<name<<", Price: $"<<price<< endl;
-}
+    // Constructor
+    Product(int i, string n, float p) {
+        id = i;
+        name = n;
+        price = p;
+        cout << "Constructor called\n";
+    }
+    ~Product() {
+        cout << "Destructor called\n";
+    }
+    void printDetails() {
+        cout << "ID: " << id << ", Name: " << name << ", Price: $" << price << endl;
+    }
+};
 
 int main() {
-  Product p(101, "Notebook", 4.99);
-  p.printDetails();
-  return 0;
+    Product p(101, "Notebook", 4.99);
+    p.printDetails();
+    return 0;
 }
